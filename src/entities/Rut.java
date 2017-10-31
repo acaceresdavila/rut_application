f1a027c29638a422061a33da25b88c378f2020ee
Marcos Emmanuel Luna <lunamarcose@gmail.com>
2017-10-31 17:58:09 -0300

    Creada entidad RUT

---
+++ src/entities/Rut.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Rut {
+	private int rut;
+	private int digitoVerificador;
+	
+	public int getRut() {
+		return rut;
+	}
+	public void setRut(int rut) {
+		this.rut = rut;
+	}
+	public int getDigitoVerificador() {
+		return digitoVerificador;
+	}
+	public void setDigitoVerificador(int digitoVerificador) {
+		this.digitoVerificador = digitoVerificador;
+	}
+}
